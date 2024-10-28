d8563032743001588a1199c794f74e5200349b00
olive <01ive@free.fr>
2024-10-28 18:53:10 +0100

    Add animations

--- beamforming.py
+++ beamforming.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def create_IQ_signal(frequence, time_vector):
     return np.exp(2j * np.pi * frequence * time_vector)
 
@@ -14,3 +15,21 @@ def generate_noise_on_signal(signals):
 
 def beamforming(signals, steering_vectors):
     return steering_vectors.conj().T @ signals
+
+def calculate_beam(tx_signal, antena_rx_nbr, antena_spacing, theta):
+    steering_vectors = create_steering_vectors(antena_rx_nbr, antena_spacing, np.radians(theta))
+
+    steering_vectors = steering_vectors.reshape(-1,1)
+    tx_signal = tx_signal.reshape(1,-1)
+    rx_signals = steering_vectors * tx_signal
+
+    rx_signals = generate_noise_on_signal(rx_signals)
+
+    theta_scan = np.linspace(-1*np.pi, np.pi, 1000)
+    results = list()
+    for theta_i in theta_scan:
+        w = create_steering_vectors(antena_rx_nbr, antena_spacing, theta_i)
+        X_weighted = beamforming(rx_signals, w)
+        results.append(10*np.log10(np.var(X_weighted)))
+    results -= np.max(results)
+    return results
